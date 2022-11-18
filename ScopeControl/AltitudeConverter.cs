cdaea059407c94efdf068993f7140899d7987565
Stefan Berg <isbeorn86@googlemail.com>
2022-11-18 12:34:11 +0100

    Add tracker annotation

---
+++ ScopeControl/AltitudeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ScopeControl {
+    public class AltitudeConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value is double dbl) {
+                return $"{Math.Round(dbl * -1, 2)}°";
+            }
+            return "--";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
